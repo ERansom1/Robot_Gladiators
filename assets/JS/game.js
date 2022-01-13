a896d0b68e0678b1cf3fff186434759e6ad5da93
ERansom1 <95004183+ERansom1@users.noreply.github.com>
2022-01-13 13:35:41 -0500

    Update game.js

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! Javascript is running")
+//Message on initial load
+//window.alert("This is an alert! Javascript is running");
+//User naming their robot
+var playerName = window.prompt("What is your robot's name?");
+windows.alert(playerName);
+
+//This creates a function named "fight"
+//Declares
+function fight(){
+    window.alert("The fight has bgeun!");
+}
+//Calls
+//fight();
